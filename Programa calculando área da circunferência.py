6b672226bfb82e8bf0bce8f434c6291c1c30717a
desjoao <98698809+desjoao@users.noreply.github.com>
2023-10-20 23:48:18 -0300

    Add files via upload

---
+++ Programa calculando área da circunferência.py
@@ -0,0 +1,4 @@
+#Programa para encontrar a área de um círculo por meio de seu raio
+print (f'Este é um programa para encontrar a área de uma circunferência por meio de seu raio.')
+raio = int(input('Insira a medida em cm do raio de uma circunferência: '))
+print (f'A área da circunferência é {raio ** 2 * 3.14} cm²')
